d4c0d145ed9d041afc54415119da0d6b4c6fc58c
Priyanshu0007 <priyanshu2859@gmail.com>
2024-02-18 13:42:57 +0530

    third section added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Hero from './components/hero'
 import Navbar from './components/navbar'
 import Second from './components/second'
+import Third from './components/third'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Navbar/>
       <Hero/>
       <Second/>
+      <Third/>
     </>
   )
 }
---
+++ src/components/third/index.jsx
@@ -0,0 +1,26 @@
+import "./style.scss"
+import { BsArrowRightCircle } from "react-icons/bs";
+
+const Third = () => {
+  return (
+    <div className="third">
+        <div className="center-line1"></div>
+        <div className="over">
+            <div className="mobile">
+                <div className="title1">Lorem ipsum</div>
+                <img alt="section-image" src="/src/assets/content-image.jpg" />
+            </div>
+            <div className="content">
+                <div className="title">Lorem ipsum</div>
+                <div className="sub-title">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+                <div className="button-title">
+                    <div><h4>Lorem ipsum</h4></div>
+                    <div><BsArrowRightCircle/></div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Third
